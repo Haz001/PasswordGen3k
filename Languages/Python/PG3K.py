9fece4bced5732d15d2fee79be941e5d1a4f32c4
Haz <master@hazonline.co.uk>
2018-03-09 10:19:28 +0000

    shortening and optomisation
    shortening it and adding one last optomisation feture

--- Languages/Python/PG3K.py
+++ Languages/Python/PG3K.py
@@ -4,28 +4,29 @@ try:
     import os
 except:
     os_i = False
-pwd = str()
+pwd = ""
 run = True
 lc_lst = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 smbl_lst = ['!','"','£','$','%','^','&','*','(',')','\\','|',',','<','.','>','[',']','{','}','_','-','+','=',':',';','@',"'",'#','~']
-while(run):
-    os.system("cls")
+while run:
+    if os_i:
+        os.system("cls")
     print("Welcome to Password Gen 3000\nTo create a password choose an option:\n0 - BasicPassword\n1 - Exit")
     try:
         cmd = int(input(">"))
-        if (cmd == 0):
+        if cmd == 0:
             print("How many letters (10 recomended)")
             try:
                 cmd = int(input(">"))
                 pwd=str()
                 count=1
-                while(count <= cmd):
+                while count <= cmd:
                     r_i = random.randint(0,3)
-                    if(r_i == 0):
+                    if r_i == 0:
                         pwd+=random.choice(lc_lst)
-                    elif(r_i == 1):
+                    elif r_i == 1:
                         pwd+=random.choice(lc_lst).upper()
-                    elif(r_i == 2):
+                    elif r_i == 2:
                         pwd+=str(random.randint(0,9))
                     else:
                         pwd+=random.choice(smbl_lst)
@@ -34,7 +35,7 @@ while(run):
                 input("Press enter to go back to main menu")
             except:
                 print("error")
-        elif (cmd == 1):
+        elif cmd == 1:
             run=False
         else:
             print("error")
